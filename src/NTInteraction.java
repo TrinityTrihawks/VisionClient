bcd544b0414b7f2452937ab660f990a58c0d8769
Charles Nykamp <cqnykamp@gmail.com>
2018-02-02 16:22:27 -0600

    Initial commit of java project

---
+++ src/NTInteraction.java
@@ -0,0 +1,18 @@
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class NTInteraction {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		NetworkTableInstance inst = NetworkTableInstance.getDefault();
+		NetworkTable table = inst.getTable("datatable");
+		NetworkTableEntry entry  = table.getEntry("X");
+		
+		
+	}
+
+}
